13c2d56ac6bec66d18cade6445a40ad89360547b
Andrii-Pershko <andriiinf@gmail.com>
2023-05-10 14:04:51 +0300

    end

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,107 +1,31 @@
 import { createSlice, isAnyOf } from '@reduxjs/toolkit';
-// Імпортуємо операцію
-import { addContacts, deleteContacts, fetchContacts } from './operations';
 
-// створюєм обєкт стейту
+import { addContacts, deleteContacts, fetchContacts } from './operations';
+import {
+  handleAddContacts,
+  handleDeleteContact,
+  handleFetchContacts,
+  handlePending,
+  handleRej,
+  processingOperations,
+} from './builderFn';
+
 const initialState = {
   items: [],
   isLoading: false,
   error: null,
 };
 
-const contactsFunctionsArr = [addContacts, deleteContacts, fetchContacts];
-
-const processingOperations = type => contactsFunctionsArr.map(fn => fn[type]);
-
-const handlePending = state => {
-  state.isLoading = true;
-};
-const handleRej = (state, action) => {
-  console.log('state', state);
-  console.log('action', action);
-
-  // тут вибиває помилку що в action нічого не приходить
-  // state.isLoading = false;
-  // state.error = action.payload;
-};
-
-const handleFetchContacts = (state, action) => {
-  state.isLoading = false;
-  state.items = action.payload;
-  state.error = null;
-};
-
-const handleDeleteContact = (state, action) => {
-  state.isLoading = false;
-  state.error = null;
-  const index = state.items.findIndex(
-    contact => contact.id === action.payload.id
-  );
-  state.items.splice(index, 1);
-};
-
-const handleAddContacts = (state, action) => {
-  state.isLoading = false;
-  state.items.push(action.payload);
-  state.error = null;
-};
-
 const contactsSlice = createSlice({
   name: 'contacts',
   initialState: initialState,
-
-  // Додаємо обробку зовнішніх екшенів
   extraReducers: builder => {
     builder
       .addCase(fetchContacts.fulfilled, handleFetchContacts)
       .addCase(addContacts.fulfilled, handleAddContacts)
       .addCase(deleteContacts.fulfilled, handleDeleteContact)
-      .addMatcher(isAnyOf(...processingOperations('pending'), handlePending))
-      // коли комітчу нижній рядок все працює але якщо повертається rejected з серверу взагалі все падає
-      .addMatcher(isAnyOf(...processingOperations('rejected'), handleRej));
-
-    //fetchContacts
-    // [fetchContacts.pending](state) {
-    //   state.isLoading = true;
-    // },
-    // [fetchContacts.fulfilled](state, action) {
-    //   state.isLoading = false;
-    //   state.items = action.payload;
-    //   state.error = null;
-    // },
-    // [fetchContacts.rejected](state, action) {
-    //   state.isLoading = false;
-    //   state.error = action.payload;
-    // },
-    //addContacts
-    // [addContacts.pending](state) {
-    //   state.isLoading = true;
-    // },
-    // [addContacts.fulfilled](state, action) {
-    //   state.isLoading = false;
-    //   state.items.push(action.payload);
-    //   state.error = null;
-    // },
-    // [addContacts.rejected](state, action) {
-    //   state.isLoading = false;
-    //   state.error = action.payload;
-    // },
-    // //deleteContacts
-    // [deleteContacts.pending](state) {
-    //   state.isLoading = true;
-    // },
-    // [deleteContacts.fulfilled](state, action) {
-    //   state.isLoading = false;
-    //   state.error = null;
-    //   const index = state.items.findIndex(
-    //     contact => contact.id === action.payload.id
-    //   );
-    //   state.items.splice(index, 1);
-    // },
-    // [deleteContacts.rejected](state, action) {
-    //   state.isLoading = false;
-    //   state.error = action.payload;
-    // },
+      .addMatcher(isAnyOf(...processingOperations('pending')), handlePending)
+      .addMatcher(isAnyOf(...processingOperations('rejected')), handleRej);
   },
 });
 

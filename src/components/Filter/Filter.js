b972f293ed701bff7b45a65a19bf8a3a94094c4e
Andrii-Pershko <andriiinf@gmail.com>
2023-05-10 10:17:01 +0300

    Update Filter.js

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,21 +1,17 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { setStatusFilter } from 'redux/filterSlice';
-import { selectContacts, selectIsLoading } from 'redux/selectors';
+import { selectContacts } from 'redux/selectors';
 
 export default function Filter() {
   const dispatch = useDispatch();
   //дізнаємось довжину масиву для того щоб знати чи рендерити компонент
   const contacts = useSelector(selectContacts).length;
-  const isLoading = useSelector(selectIsLoading);
 
   //змінюємо setStatusFilter в залежності від значення
   const handleFilterChange = e => {
     dispatch(setStatusFilter(e.target.value));
   };
 
-  if (isLoading) {
-    return;
-  }
   return (
     <>
       {/* якщо масив контактів порожній виводимо надпис про це, інакше показуємо фільтр*/}

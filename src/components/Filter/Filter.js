52353b048843c23c80e4f2ed408e39fa43e815b3
Andrii-Pershko <andriiinf@gmail.com>
2023-05-13 14:42:54 +0300

    Update Filter.js

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,4 +1,4 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { useGetContactsQuery } from 'redux/contactsApi';
 import { setStatusFilter } from 'redux/filterSlice';
 
